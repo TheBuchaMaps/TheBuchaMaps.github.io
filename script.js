5568e70b95f288c60ed01a3f3cc9c697c022707b
TheBuchaMaps <102299522+TheBuchaMaps@users.noreply.github.com>
2022-08-13 21:14:15 +0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    $('head').prepend('<meta http-equiv="refresh" content="0; url=Mobile/index.html">')
-} else {
-    $('head').prepend('<meta http-equiv="refresh" content="0; url=Desktop/index.html">')
-}
